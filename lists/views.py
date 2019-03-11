ba16b104afeab0a9a798374232959f2ec6378845
yamen <yamen225@gmail.com>
2019-03-11 15:49:53 +0200

    New Model list created

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import Item
+from .models import Item, List
 
 
 def home_page(request):
@@ -9,10 +9,12 @@ def home_page(request):
 
 
 def view_list(request):
-    
+
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
 
+
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
