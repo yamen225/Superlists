389c6367cfe0eacd0efcc50ae816ea355ac0bf60
yamen <yamen225@gmail.com>
2019-03-11 07:02:57 +0200

    activating post request

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -20,10 +21,9 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser = self.browser.find_element_by_id(
+        inputbox = self.browser.find_element_by_id(
             'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
@@ -31,7 +31,7 @@ class NewVisitorTest(unittest.TestCase):
         )
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        inputbox.send_keys('buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list.
@@ -40,12 +40,23 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly"
         # (Edith is very methodical)
+        inputbox = self.browser.find_element_by_id(
+            'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
+
         self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list.
