239ba40128f9df61afdd67776c86ad589ed50c94
yamen <yamen225@gmail.com>
2019-03-20 05:36:45 +0200

    hack ALLOWED HOSTS for everyone to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2_lpg&xi^#=f)*q9@+nivd1h!u(np5%&-$i(8p5fspgnzex*!k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
